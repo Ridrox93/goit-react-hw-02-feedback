b4d032e6778da809f9cd471bd2435c66cb064d43
Ridrox93 <popovandrii@yahoo.com>
2023-10-23 21:17:03 +0200

    feedback

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Section = ({ title, children }) => (
+  <section>
+    <h2>{title}</h2>
+    {children}
+  </section>
+);
+
+export const Notification = ({ message }) => <p>{message}</p>;
